31f478f8c6d067dd87864028d525b1af29fd91b1
Coval1000 <michal1kow@gmail.com>
2019-08-06 19:16:25 +0200

    Fixed user path

--- shaders/compile.sh
+++ shaders/compile.sh
@@ -1,2 +1,2 @@
-/home/daniel/VulkanSDK/1.1.114.0/x86_64/bin/glslc shader.vert -o vert.spv
-/home/daniel/VulkanSDK/1.1.114.0/x86_64/bin/glslc shader.frag -o frag.spv
+/home/$USER/VulkanSDK/1.1.114.0/x86_64/bin/glslc shader.vert -o vert.spv
+/home/$USER/VulkanSDK/1.1.114.0/x86_64/bin/glslc shader.frag -o frag.spv
