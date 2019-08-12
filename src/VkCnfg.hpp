748404f1ad2939002caa9e3ab23aac736f04b0a6
Coval1000 <michal1kow@gmail.com>
2019-08-12 10:00:04 +0200

    .vs rule

---
+++ src/VkCnfg.hpp
@@ -0,0 +1,155 @@
+#pragma once
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <fstream>
+#include <stdexcept>
+#include <algorithm>
+#include <vector>
+#include <cstring>
+#include <cstdlib>
+#include <optional>
+#include <set>
+#include <glm/glm.hpp>
+#include <array>
+
+const int WIDTH = 800;
+const int HEIGHT = 600;
+
+const int MAX_FRAMES_IN_FLIGHT = 2;
+
+namespace
+{
+	const std::vector<const char*> deviceExtensions = { VK_KHR_SWAPCHAIN_EXTENSION_NAME };
+
+	struct QueueFamilyIndices
+	{
+		std::optional<uint32_t> graphicsFamily;
+		std::optional<uint32_t> presentFamily;
+
+		bool isComplete()
+		{
+			return graphicsFamily.has_value() && presentFamily.has_value();
+		}
+	};
+
+	struct SwapChainSupportDetails
+	{
+		VkSurfaceCapabilitiesKHR capabilities;
+		std::vector<VkSurfaceFormatKHR> formats;
+		std::vector<VkPresentModeKHR> presentModes;
+	};
+}
+
+class Vulkan
+{
+public:
+	Vulkan();
+	typedef struct Vertex
+	{
+		glm::vec2 pos;
+		glm::vec3 color;
+
+		static VkVertexInputBindingDescription getBindingDestription()
+		{
+			VkVertexInputBindingDescription bindingDestription = {};
+			bindingDestription.binding = 0;
+			bindingDestription.stride = sizeof(Vertex);
+			bindingDestription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+
+			return bindingDestription;
+		}
+
+		static std::array<VkVertexInputAttributeDescription, 2> getAttributeDescriptions()
+		{
+			std::array<VkVertexInputAttributeDescription, 2> attributeDesctription = {};
+			attributeDesctription[0].binding = 0;
+			attributeDesctription[0].location = 0;
+			attributeDesctription[0].format = VK_FORMAT_R32G32_SFLOAT;
+			attributeDesctription[0].offset = offsetof(Vertex, pos);
+
+			attributeDesctription[1].binding = 0;
+			attributeDesctription[1].location = 1;
+			attributeDesctription[1].format = VK_FORMAT_R32G32B32_SFLOAT;
+			attributeDesctription[1].offset = offsetof(Vertex, color);
+
+			return attributeDesctription;
+		}
+	} Vertex;
+
+	void initVulkan(VkInstance*, VkSurfaceKHR*);
+	void drawFrame();
+	void shutdown();
+	void createCommandBuffers(VkBuffer, std::vector<Vertex>);
+	VkDevice getDevice();
+	uint32_t findMemoryType(uint16_t, VkMemoryPropertyFlags);
+
+private:
+	VkInstance *instance;
+	VkSurfaceKHR *surface;
+	VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+	VkDevice device;
+
+	VkQueue graphicsQueue;
+	VkQueue presentQueue;
+
+	VkSwapchainKHR swapChain;
+	std::vector<VkImage> swapChainImages;
+	VkFormat swapChainImageFormat;
+	VkExtent2D swapChainExtent;
+	std::vector<VkImageView> swapChainImageViews;
+	std::vector<VkFramebuffer> swapChainFramebuffers;
+
+	VkRenderPass renderPass;
+	VkPipelineLayout pipelineLayout;
+	VkPipeline graphicsPipeline;
+
+	VkCommandPool commandPool;
+	std::vector<VkCommandBuffer> commandBuffers;
+
+	std::vector<VkSemaphore> imageAvailableSemaphores;
+	std::vector<VkSemaphore> renderFinishedSemaphores;
+	std::vector<VkFence> inFlightFences;
+	size_t currentFrame = 0;
+
+
+	void pickPhysicalDevice();
+	void createLogicalDevice();
+	void createSwapChain();
+	void createImageViews();
+	void createRenderPass();
+	void createGraphicsPipeline();
+	void createFramebuffers();
+	void createCommandPool();
+	
+	void createSyncObjects();
+
+	VkShaderModule createShaderModule(const std::vector<char>&);
+	VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>&);
+	VkPresentModeKHR chooseSwapPresentMode(const std::vector<VkPresentModeKHR>&);
+	VkExtent2D chooseSwapExtent(const VkSurfaceCapabilitiesKHR&);
+	bool isDeviceSuitable(VkPhysicalDevice);
+	QueueFamilyIndices findQueueFamilies(VkPhysicalDevice);
+	bool checkDeviceExtensionSupport(VkPhysicalDevice);
+	SwapChainSupportDetails querySwapChainSupport(VkPhysicalDevice);
+
+	static std::vector<char> readFile(const std::string& filename)
+	{
+		std::ifstream file(filename, std::ios::ate | std::ios::binary);
+		if (!file.is_open())
+		{
+			throw std::runtime_error("failed to open file!");
+		}
+		size_t fileSize = (size_t)file.tellg();
+		std::vector<char> buffer(fileSize);
+
+		file.seekg(0);
+		file.read(buffer.data(), fileSize);
+		file.close();
+
+		return buffer;
+	}
+
+};
